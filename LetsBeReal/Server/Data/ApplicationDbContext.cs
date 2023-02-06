942963ee2fe7f968485e8fc639e3a9a0d3d86c7e
Reshman Kumaar <116149016+RK111104@users.noreply.github.com>
2023-02-06 20:16:12 +0800

    Update ApplicationDbContext.cs

--- LetsBeReal/Server/Data/ApplicationDbContext.cs
+++ LetsBeReal/Server/Data/ApplicationDbContext.cs
@@ -1,5 +1,8 @@
-﻿using IdentityServer4.EntityFramework.Options;
+using IdentityServer4.EntityFramework.Options;
+using LetsBeReal.Server.Configurations.Entities;
 using LetsBeReal.Server.Models;
+using LetsBeReal.Shared;
+using LetsBeReal.Shared.Domain;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
@@ -17,5 +20,24 @@ namespace LetsBeReal.Server.Data
             IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
         {
         }
+
+        public DbSet<Reviews> Review { get; set; }
+        public DbSet<Genre> Genre { get; set; }
+        public DbSet<Update> News { get; set; }
+        public DbSet<UserInterest> Interests { get; set; }
+        public DbSet<Games> Game { get; set; }
+        public DbSet<Product> TechStore { get; set; }
+        public DbSet<UserRank> UserRank { get; set; }
+        public DbSet<User> User { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new ReviewConfiguration());
+            builder.ApplyConfiguration(new GameConfiguration());
+            builder.ApplyConfiguration(new GenreConfiguration());
+            builder.ApplyConfiguration(new UserConfiguration());
+        }
+
     }
 }
