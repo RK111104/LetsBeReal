80bb43105b0863bf185d650368bc70bbcd99ed94
Reshman Kumaar <116149016+RK111104@users.noreply.github.com>
2023-02-06 20:23:34 +0800

    Add files via upload

---
+++ LetsBeReal/Server/IRepository/IUnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using LetsBeReal.Shared;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace LetsBeReal.Server.IRepository
+{
+  
+        public interface IUnitOfWork : IDisposable
+        {
+            Task Save(HttpContext httpContext);
+            IGenericRepository<Games> Gamess { get; }
+            IGenericRepository<Genre> Genres { get; }
+            IGenericRepository<Reviews> Reviewss { get; }
+            IGenericRepository<Product> Products { get; }
+            IGenericRepository<UserInterest> UserInterests { get; }
+            IGenericRepository<UserRank> UserRanks { get; }
+            IGenericRepository<Update> Updates { get; }
+            IGenericRepository<User> Users { get; }
+        }
+}
+
+
